12f90eb57557afd37ebe471d7d26ecef7857e648
VinisshaaK <70569437+VinisshaaK@users.noreply.github.com>
2021-12-01 11:13:24 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+/*--importing routes*/
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+
+
+const { METHODS } = require('http');
+app.use(cors({
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: 'Content-type, Authoriztion, Origin, X-requested-width, Accept'
+
+}))
+
+//use routes
+
+app.use('/api/products', productsRoute);
+app.use('/api/orders', ordersRoute);
+
+app.use(express.static(root : __dirname + '/dist'));
+
+
+
+app.listen(port: process.env.PORT || 3000, callback: function () {
+  console.log("listening on http://localhost:3000");
+});
+
+module.exports = app;
